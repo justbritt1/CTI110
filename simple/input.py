b823c0e457fc6d2a503b98d1f8dd5c892279e3df
Brittany <107373479+justbritt1@users.noreply.github.com>
2026-09-01 18:03:31 +0000

    funnzies

---
+++ simple/input.py
@@ -0,0 +1,11 @@
+# Brittany Smith
+# input practice
+# cool
+
+print("Hello!")
+
+#first_name = input("Enter your first name: ")
+#last_name = input("Enter your last name: ")
+
+my_name = "Brittany Smith"
+print(my_name)

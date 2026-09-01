cb710000b2abdea72e9392af6b0e816fa35a38d4
Brittany <107373479+justbritt1@users.noreply.github.com>
2026-09-01 18:21:50 +0000

    new change

--- simple/input.py
+++ simple/input.py
@@ -4,8 +4,7 @@
 
 print("Hello!")
 
-#first_name = input("Enter your first name: ")
-#last_name = input("Enter your last name: ")
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
 
-my_name = "Brittany Smith"
-print(my_name)
+print("Nice to meet you", first_name, last_name,)
